9e52ed020c0246faddb4db6a63aca3c55acbafa7
Rader <raderpy@protonmail.com>
2023-02-14 16:30:34 +0100

    made Reigns.java an actual class

--- app/src/main/java/redder/reigns/Reigns.java
+++ app/src/main/java/redder/reigns/Reigns.java
@@ -1,34 +1,20 @@
 package redder.reigns;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-/**
- * La classe Reigns représente le programme principal du jeu Reigns
- *
- * @author Julie Jacques / Lucien Mousin
- * @version 1.0
- */
-public class Reigns {
-    /**
-     * le personnage joué
-     */
-    private static Personnage personnage;
+public final class Reigns {
 
-    /**
-     * la banque de questions
-     */
-    private static ArrayList<Question> questions;
+    private final List<Question> questions;
 
-    /**
-     * La méthode main lance le jeu Reigns. Il initialise les questions, le personnage,
-     * affiche les jauges du personnage et lance une boucle de jeu qui se termine lorsque le personnage perd.
-     * Il affiche également le nombre de tours de jeu que le personnage a joué.
-     *
-     * @param args les arguments de la ligne de commande
-     */
-    public static void main(String[] args){
+    private Personnage personnage;
 
+    private Reigns() {
+        this.questions = new ArrayList<>();
+    }
+
+    private void start() {
         // début du jeu
         System.out.println("Bienvenue sur Reigns");
 
@@ -38,36 +24,29 @@ public class Reigns {
 
         initPersonnage();
 
-        System.out.println(personnage.getGenre().longRegne()
-                +" "+personnage.getNom());
+        System.out.println(this.personnage.getGenre().longRegne()
+                + " " + this.personnage.getNom());
 
-        personnage.AfficheJauges();
+        this.personnage.AfficheJauges();
 
         // tirage des questions
         int nbTours = 0;
-        while(!personnage.finDuJeu()){
+        while (!this.personnage.finDuJeu()) {
             nbTours++;
             Question question = getQuestionAleatoire();
             reponseQuestion(question);
-            personnage.AfficheJauges();
+            this.personnage.AfficheJauges();
         }
 
         // fin du jeu
         System.out.println(
-                personnage.getNom()
+                this.personnage.getNom()
                         + " a perdu ! Son règne a duré "
-                        +nbTours
+                        + nbTours
                         + " tours");
-
     }
 
-    /**
-     * Cette fonction permet de gérer la réponse à une question donnée. Elle affiche la question, demande à
-     * l'utilisateur d'entrer une réponse (soit "G" soit "D") et en fonction de la réponse, elle appelle la méthode
-     * appropriée pour appliquer les conséquences sur les jauges du personnage.
-     * @param question La question à laquelle il faut répondre
-     */
-    private static void reponseQuestion(Question question){
+    private void reponseQuestion(Question question) {
         question.afficheQuestion();
         // récupère la réponse
         Scanner scanner = new Scanner(System.in);
@@ -79,18 +58,13 @@ public class Reigns {
         }
         // applique les malus
         if(reponse.equals("G")){
-            question.appliqueEffetsGauche(personnage);
+            question.appliqueEffetsGauche(this.personnage);
         }else{
-            question.appliqueEffetsDroite(personnage);
+            question.appliqueEffetsDroite(this.personnage);
         }
     }
 
-    /**
-     * Cette fonction permet d'initialiser le personnage joué. Elle demande à l'utilisateur de saisir le nom du personnage
-     * et le genre (Roi ou Reine). Elle crée ensuite le personnage.
-     */
-
-    private static void initPersonnage(){
+    private void initPersonnage() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Entrez le nom du personnage: ");
         System.out.flush();
@@ -105,14 +79,10 @@ public class Reigns {
             roiReine = Genre.REINE;
         }
 
-        Reigns.personnage = new Personnage(nom,roiReine);
+        this.personnage = new Personnage(nom,roiReine);
     }
 
-    /**
-     * Cette fonction initialise la banque de questions. Elle crée les questions et les ajoute à la banque.
-     */
-    private static void initBanqueQuestions(){
-        questions = new ArrayList<>();
+    private void initBanqueQuestions() {
         Question question1 = new Question(
                 "Main du roi",
                 "Le peuple souhaite libérer les prisonniers",
@@ -121,7 +91,8 @@ public class Reigns {
         question1.ajouteEffetGauche(TypeJauge.ARMEE, -5);
         question1.ajouteEffetGauche(TypeJauge.PEUPLE, +5);
         question1.ajouteEffetDroite(TypeJauge.PEUPLE, -7);
-        questions.add(question1);
+        this.questions.add(question1);
+
         Question question2 = new Question(
                 "Paysan",
                 "Il n'y a plus rien à manger",
@@ -130,7 +101,8 @@ public class Reigns {
         question2.ajouteEffetGauche(TypeJauge.FINANCE,-5);
         question2.ajouteEffetGauche(TypeJauge.PEUPLE, +5);
         question2.ajouteEffetDroite(TypeJauge.PEUPLE, -5);
-        questions.add(question2);
+        this.questions.add(question2);
+
         Question question3 = new Question(
                 "Prêtre",
                 "Les dieux sont en colère",
@@ -139,7 +111,8 @@ public class Reigns {
         question3.ajouteEffetGauche(TypeJauge.CLERGE, +5);
         question3.ajouteEffetGauche(TypeJauge.PEUPLE, -3);
         question3.ajouteEffetDroite(TypeJauge.CLERGE, -5);
-        questions.add(question3);
+        this.questions.add(question3);
+
         Question question4 = new Question(
                 "Main du roi",
                 "Le roi Baratheon rassemble son armée",
@@ -149,7 +122,8 @@ public class Reigns {
         question4.ajouteEffetGauche(TypeJauge.FINANCE, -3);
         question4.ajouteEffetGauche(TypeJauge.CLERGE, -3);
         question4.ajouteEffetDroite(TypeJauge.PEUPLE, +3);
-        questions.add(question4);
+        this.questions.add(question4);
+
         Question question5 = new Question(
                 "Paysan",
                 "Abondance de récoltes cette année",
@@ -159,15 +133,16 @@ public class Reigns {
         question5.ajouteEffetGauche(TypeJauge.PEUPLE, -5);
         question5.ajouteEffetDroite(TypeJauge.FINANCE, +1);
         question5.ajouteEffetDroite(TypeJauge.PEUPLE, -3);
-        questions.add(question5);
+        this.questions.add(question5);
     }
 
-    /**
-     * Cette fonction permet de tirer une question aléatoire dans la banque de questions.
-     * @return Une question aléatoire
-     */
-    private static Question getQuestionAleatoire(){
-        int numQuestion = (int) (Math.random()*questions.size());
-        return questions.get(numQuestion);
+    private Question getQuestionAleatoire(){
+        int numQuestion = (int) (Math.random()*this.questions.size());
+        return this.questions.get(numQuestion);
+    }
+
+    public static void main(String[] args) {
+        Reigns reigns = new Reigns();
+        reigns.start();
     }
 }
