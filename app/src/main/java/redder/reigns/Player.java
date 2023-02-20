e34877081442a0813326a47cd5b374daacc57a36
Rader <raderpy@protonmail.com>
2023-02-20 14:28:48 +0100

    created player class

---
+++ app/src/main/java/redder/reigns/Player.java
@@ -0,0 +1,35 @@
+package redder.reigns;
+
+public class Player {
+
+    private final String name;
+    private final Gender gender;
+
+    public Player(String name, Gender gender) {
+        this.name = name;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Gender getGender() {
+        return this.gender;
+    }
+
+    enum Gender {
+        KING("Long règne au roi"),
+        QUEEN("Long règne à la reigne");
+
+        private final String longReignText;
+
+        Gender(String longReignText) {
+            this.longReignText = longReignText;
+        }
+
+        public String longReign() {
+            return this.longReignText;
+        }
+    }
+}
