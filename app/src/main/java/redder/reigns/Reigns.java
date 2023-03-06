ff82f647e960718c905074fd3350488ddcf34d4d
Rader <raderpy@protonmail.com>
2023-03-06 19:16:31 +0100

    moved code from TestMain to Reigns class

--- app/src/main/java/redder/reigns/Reigns.java
+++ app/src/main/java/redder/reigns/Reigns.java
@@ -1,148 +1,93 @@
 package redder.reigns;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import redder.reigns.effects.Effect;
+import redder.reigns.gauges.Gauge;
+import redder.reigns.gauges.GaugePool;
+import redder.reigns.questions.Question;
+import redder.reigns.questions.QuestionPool;
+import redder.reigns.utils.Input;
 
 public final class Reigns {
 
-    private final List<Question> questions;
-
-    private Personnage personnage;
+    private final QuestionPool questionPool;
+    private final GaugePool gaugePool;
 
     private Reigns() {
-        this.questions = new ArrayList<>();
+        this.questionPool = QuestionPool.getInstance();
+        this.gaugePool = GaugePool.getInstance();
     }
 
     private void start() {
-        // début du jeu
         System.out.println("Bienvenue sur Reigns");
-
-        initBanqueQuestions();
-
         System.out.println("Création du personnage...");
 
-        initPersonnage();
+        Player player = initPlayer();
 
-        System.out.println(this.personnage.getGenre().longRegne()
-                + " " + this.personnage.getNom());
+        System.out.println(player.getGender().getLongReignText());
 
-        this.personnage.AfficheJauges();
-
-        // tirage des questions
         int nbTours = 0;
-        while (!this.personnage.finDuJeu()) {
+        while (!this.gaugePool.hasFilledOrEmptyGauge()) {
+            this.gaugePool.printGauges();
+
+            showRandomQuestion();
+
             nbTours++;
-            Question question = getQuestionAleatoire();
-            reponseQuestion(question);
-            this.personnage.AfficheJauges();
         }
 
-        // fin du jeu
+        this.gaugePool.printGauges();
+
         System.out.println(
-                this.personnage.getNom()
-                        + " a perdu ! Son règne a duré "
-                        + nbTours
-                        + " tours");
+                player.getName() + " a perdu ! " +
+                        "Son règne a duré " + nbTours + " tours"
+        );
     }
 
-    private void reponseQuestion(Question question) {
-        question.afficheQuestion();
-        // récupère la réponse
-        Scanner scanner = new Scanner(System.in);
-        String reponse = "";
-        while(!reponse.equals("G") && !reponse.equals("D")){
-            System.out.println("Entrez la réponse (G ou D)");
-            System.out.flush();
-            reponse = scanner.nextLine();
-        }
-        // applique les malus
-        if(reponse.equals("G")){
-            question.appliqueEffetsGauche(this.personnage);
-        }else{
-            question.appliqueEffetsDroite(this.personnage);
-        }
+    private void showRandomQuestion() {
+        Question question = this.questionPool.getRandomQuestion();
+        question.printQuestion();
+
+        Effect.Direction[] directions = Effect.Direction.values();
+        printEnumEntries(directions);
+
+        int answer = Input.readClampedInt(1, directions.length) - 1;
+        Effect.Direction selectedDirection = directions[answer];
+
+        question.getEffects()
+                .stream()
+                .filter(effect -> effect.getDirection().equals(selectedDirection))
+                .forEach(effect -> {
+                    Gauge gauge = gaugePool.getGaugeByType(effect.getAffectedGauge());
+                    gauge.updateValue(effect.getStrength());
+                });
     }
 
-    private void initPersonnage() {
-        Scanner scanner = new Scanner(System.in);
+    private Player initPlayer() {
         System.out.println("Entrez le nom du personnage: ");
-        System.out.flush();
-        String nom = scanner.nextLine();
-        System.out.println(
-                "Faut-il vous appeler Roi ou Reine ? (1 pour Roi, 2 pour Reine)");
-        int genre = scanner.nextInt();
-        Genre roiReine;
-        if(genre==1){
-            roiReine = Genre.ROI;
-        }else{
-            roiReine = Genre.REINE;
+        String nom = Input.readString();
+
+        System.out.println("Comment faut-il vous appeler?");
+
+        Player.Gender[] genders = Player.Gender.values();
+        printEnumEntries(genders);
+
+        int gender = Input.readClampedInt(1, genders.length) - 1;
+        return new Player(nom, genders[gender]);
+    }
+
+    private <T extends Enum<?>> void printEnumEntries(T[] enumeration) {
+        for (int i = 1; i <= enumeration.length; i++) {
+            System.out.print(i + " pour " + enumeration[i - 1]);
+
+            if (i < enumeration.length) {
+                System.out.print(", ");
+            }
         }
 
-        this.personnage = new Personnage(nom,roiReine);
-    }
-
-    private void initBanqueQuestions() {
-        Question question1 = new Question(
-                "Main du roi",
-                "Le peuple souhaite libérer les prisonniers",
-                "Oui",
-                "Non");
-        question1.ajouteEffetGauche(TypeJauge.ARMEE, -5);
-        question1.ajouteEffetGauche(TypeJauge.PEUPLE, +5);
-        question1.ajouteEffetDroite(TypeJauge.PEUPLE, -7);
-        this.questions.add(question1);
-
-        Question question2 = new Question(
-                "Paysan",
-                "Il n'y a plus rien à manger",
-                "Importer de la nourriture",
-                "Ne rien faire");
-        question2.ajouteEffetGauche(TypeJauge.FINANCE,-5);
-        question2.ajouteEffetGauche(TypeJauge.PEUPLE, +5);
-        question2.ajouteEffetDroite(TypeJauge.PEUPLE, -5);
-        this.questions.add(question2);
-
-        Question question3 = new Question(
-                "Prêtre",
-                "Les dieux sont en colère",
-                "Faire un sacrifice",
-                "Ne rien faire");
-        question3.ajouteEffetGauche(TypeJauge.CLERGE, +5);
-        question3.ajouteEffetGauche(TypeJauge.PEUPLE, -3);
-        question3.ajouteEffetDroite(TypeJauge.CLERGE, -5);
-        this.questions.add(question3);
-
-        Question question4 = new Question(
-                "Main du roi",
-                "Le roi Baratheon rassemble son armée",
-                "Le soutenir",
-                "Rester neutre");
-        question4.ajouteEffetGauche(TypeJauge.ARMEE, +3);
-        question4.ajouteEffetGauche(TypeJauge.FINANCE, -3);
-        question4.ajouteEffetGauche(TypeJauge.CLERGE, -3);
-        question4.ajouteEffetDroite(TypeJauge.PEUPLE, +3);
-        this.questions.add(question4);
-
-        Question question5 = new Question(
-                "Paysan",
-                "Abondance de récoltes cette année",
-                "Taxer énormément",
-                "Taxer un tout petit peu");
-        question5.ajouteEffetGauche(TypeJauge.FINANCE, +10);
-        question5.ajouteEffetGauche(TypeJauge.PEUPLE, -5);
-        question5.ajouteEffetDroite(TypeJauge.FINANCE, +1);
-        question5.ajouteEffetDroite(TypeJauge.PEUPLE, -3);
-        this.questions.add(question5);
-    }
-
-    private Question getQuestionAleatoire(){
-        int numQuestion = (int) (Math.random()*this.questions.size());
-        return this.questions.get(numQuestion);
+        System.out.println();
     }
 
     public static void main(String[] args) {
-        Reigns reigns = new Reigns();
-        reigns.start();
+        Reigns main = new Reigns();
+        main.start();
     }
 }
